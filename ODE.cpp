2d59eac654c0f92e11eda60b1653cb9df461ad4d
JADC362 <32496764+JADC362@users.noreply.github.com>
2018-11-13 17:54:05 -0500

    Add files via upload

---
+++ ODE.cpp
@@ -0,0 +1,11 @@
+//Importacion de librerias
+#include<iostream>
+#include<cmath>
+
+//Definicion de variables globales
+float static c = 0.2;
+float static m  = 0.2;
+float static g = {0,10};
+
+std:cout << g+g << std:end;
+
