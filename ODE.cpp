902ca36afd6fe7471653b8256ec5f3afa57bf10d
JADC362 <32496764+JADC362@users.noreply.github.com>
2018-11-13 21:05:19 -0500

    Add files via upload

--- ODE.cpp
+++ ODE.cpp
@@ -4,19 +4,20 @@
 #include <vector>
 #include <math.h> 
 using namespace std;
+
 //Definicion de variables globales
 //Como se hace uso de vectores, la convencion de componentes sera sera A = {Ax,Ay}
 float static c = 0.2;
 float static m  = 0.2;
-//float static g[2] = {0.0,10.0};
+float static PI = 3.1415;
 vector<float> g;
-int static ht = 0.01; //Intervalo de tiempo entre punto y punto
-float static timeS = 5; //Tiempo total de simulacion
+float static ht = 0.01; //Intervalo de tiempo entre punto y punto
+float static timeS = 5.0; //Tiempo total de simulacion
 int N = (int) timeS/ht; 
 
 vector<float> f[2];
 
-void funY(vector<float> y0,vector<float> y1){
+void calcularFuncion(vector<float> y0,vector<float> y1){
 	float magY1 = sqrt(pow(y1.at(0),2)+pow(y1.at(1),2));
 	vector<float> tempY1;
 	float tempY1_x = -g.at(0)-(c/m)*(magY1)*y1.at(0);
@@ -28,7 +29,7 @@ void funY(vector<float> y0,vector<float> y1){
 }
 
 void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
-	//Inicianliazdo y asignando el valor inicial a Y = {X,V}
+	//Inicializando y asignando el valor inicial a Y = {X,V}
 	vector<float> Y[N][2];
 	Y[0][0]=x0;
 	Y[0][1]=v0;
@@ -37,7 +38,7 @@ void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
 	{
 		//Calculo de k1
 		vector<float> k1[2];
-		funY(Y[i-1][0],Y[i-1][1]);
+		calcularFuncion(Y[i-1][0],Y[i-1][1]);
 		k1[0] = f[0];
 		k1[1] = f[1];
 
@@ -45,18 +46,18 @@ void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
 		vector<float> k2[2];
 
 		vector<float> tempY0_k2;
-		float tempY0_k2_x = (Y[i-1][0]).at(0)+(ht/2);
-		float tempY0_k2_y = (Y[i-1][0]).at(1)+(ht/2);
+		float tempY0_k2_x = (Y[i-1][0]).at(0)+(ht/2.0);
+		float tempY0_k2_y = (Y[i-1][0]).at(1)+(ht/2.0);
 		tempY0_k2.push_back(tempY0_k2_x);
 		tempY0_k2.push_back(tempY0_k2_y);
 
 		vector<float> tempY1_k2;
-		float tempY1_k2_x = (Y[i-1][1]).at(0)+((k1[1]).at(0))*(ht/2);
-		float tempY1_k2_y = (Y[i-1][1]).at(1)+((k1[1]).at(1))*(ht/2);
+		float tempY1_k2_x = (Y[i-1][1]).at(0)+((k1[1]).at(0))*(ht/2.0);
+		float tempY1_k2_y = (Y[i-1][1]).at(1)+((k1[1]).at(1))*(ht/2.0);
 		tempY1_k2.push_back(tempY1_k2_x);
 		tempY1_k2.push_back(tempY1_k2_y);
 
-		funY(tempY0_k2,tempY1_k2);
+		calcularFuncion(tempY0_k2,tempY1_k2);
 		k2[0] = f[0];
 		k2[1] = f[1];
 
@@ -64,18 +65,18 @@ void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
 		vector<float> k3[2];
 
 		vector<float> tempY0_k3;
-		float tempY0_k3_x = (Y[i-1][0]).at(0)+(ht/2);
-		float tempY0_k3_y = (Y[i-1][0]).at(1)+(ht/2);
+		float tempY0_k3_x = (Y[i-1][0]).at(0)+(ht/2.0);
+		float tempY0_k3_y = (Y[i-1][0]).at(1)+(ht/2.0);
 		tempY0_k3.push_back(tempY0_k3_x);
 		tempY0_k3.push_back(tempY0_k3_y);
 
 		vector<float> tempY1_k3;
-		float tempY1_k3_x = (Y[i-1][1]).at(0)+((k2[1]).at(0))*(ht/2);
-		float tempY1_k3_y = (Y[i-1][1]).at(1)+((k2[1]).at(1))*(ht/2);
+		float tempY1_k3_x = (Y[i-1][1]).at(0)+((k2[1]).at(0))*(ht/2.0);
+		float tempY1_k3_y = (Y[i-1][1]).at(1)+((k2[1]).at(1))*(ht/2.0);
 		tempY1_k3.push_back(tempY1_k3_x);
 		tempY1_k3.push_back(tempY1_k3_y);
 
-		funY(tempY0_k3,tempY1_k3);
+		calcularFuncion(tempY0_k3,tempY1_k3);
 		k3[0] = f[0];
 		k3[1] = f[1];
 
@@ -94,7 +95,7 @@ void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
 		tempY1_k4.push_back(tempY1_k4_x);
 		tempY1_k4.push_back(tempY1_k4_y);
 
-		funY(tempY0_k4,tempY1_k4);
+		calcularFuncion(tempY0_k4,tempY1_k4);
 		k4[0] = f[0];
 		k4[1] = f[1];
 
@@ -123,6 +124,10 @@ void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
 		Y[i][0]=Y0;
 		Y[i][1]=Y1;
 	}
+	for (int i = 0; i < N; ++i)
+	{
+		cout<<"X-x="<<(Y[i][0]).at(0)<<"  ,  X_y="<<(Y[i][0]).at(1)<<endl;
+	}
 }
 
 int main(){
@@ -134,9 +139,9 @@ int main(){
 	x0.push_back(0.0);
 
 	vector<float> v0;
-	v0.push_back(300*cos(45));
-	v0.push_back(300*sin(45));
+	v0.push_back(300*cos(45*PI/180.0));
+	v0.push_back(300*sin(45*PI/180.0));
 
-	//calcularMovimientoProyectil(x0,v0);
+	calcularMovimientoProyectil(x0,v0);
 	return 0;
 }
