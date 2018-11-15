8f9bacbfbd0d472267032c8a4876ffce00c00924
JADC362 <32496764+JADC362@users.noreply.github.com>
2018-11-14 20:04:30 -0500

    Add files via upload

--- ODE.cpp
+++ ODE.cpp
@@ -1,6 +1,9 @@
 //Importacion de librerias
 #include<iostream>
+#include <fstream>
 #include<cmath>
+#include <sstream>
+#include <algorithm> 
 #include <vector>
 #include <math.h> 
 using namespace std;
@@ -16,6 +19,8 @@ float static timeS = 3.0; //Tiempo total de simulacion
 int N = (int) timeS/ht; 
 
 vector<float> f[2];
+vector<float> x0;
+vector<float> v0;
 
 void calcularFuncion(vector<float> y0,vector<float> y1){
 	float magY1 = sqrt(pow(y1.at(0),2)+pow(y1.at(1),2));
@@ -28,12 +33,26 @@ void calcularFuncion(vector<float> y0,vector<float> y1){
 	f[1]=tempY1;
 }
 
-void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
+float calcularMovimientoProyectil(float angulo,string archivoNombre){
+	//Condiciones iniciales para V0
+	v0.clear();
+	v0.push_back(300*cos(angulo*PI/180.0));
+	v0.push_back(300*sin(angulo*PI/180.0));
+
 	//Inicializando y asignando el valor inicial a Y = {X,V}
 	vector<float> Y[N][2];
 	Y[0][0]=x0;
 	Y[0][1]=v0;
 
+	//Creacion flujo de datos para archivo datos.dat
+	ofstream datos;
+	datos.open(archivoNombre.c_str());
+
+	datos<<"Angulo:"<<angulo<<endl;
+
+	float distanciaRecorrida = 0;
+
+	//Calculo de movimiento y velocidad del proyectil
 	for (int i = 1; i < N; ++i)
 	{
 		//Calculo de k1
@@ -129,23 +148,45 @@ void calcularMovimientoProyectil(vector<float> x0,vector<float> v0){
 		Y[i][0]=Y0;
 		Y[i][1]=Y1;
 
-		//cout<<"X-x="<<(Y[i-1][0]).at(0)<<"  ,  X_y="<<(Y[i-1][0]).at(1)<<"  --  V-x="<<(Y[i-1][1]).at(0)<<"  ,  V_y="<<(Y[i-1][1]).at(1)<<endl;
-		cout<<(Y[i-1][0]).at(0)<<","<<(Y[i-1][0]).at(1)<<","<<(Y[i-1][1]).at(0)<<","<<(Y[i-1][1]).at(1)<<endl;
+		distanciaRecorrida += sqrt(pow(Y[i][0].at(0)-Y[i-1][0].at(0),2)+pow(Y[i][0].at(1)-Y[i-1][0].at(1),2));
+
+		
+		datos<<"t="<<i*ht<<" , X-x="<<(Y[i-1][0]).at(0)<<" , X_y="<<(Y[i-1][0]).at(1)<<" , V-x="<<(Y[i-1][1]).at(0)<<" , V_y="<<(Y[i-1][1]).at(1)<<"\n";
 	}
+	datos.close();
+	return distanciaRecorrida;
 }
 
 int main(){
 	g.push_back(0.0);
 	g.push_back(10.0);	
 
-	vector<float> x0;
 	x0.push_back(0.0);
 	x0.push_back(0.0);
 
-	vector<float> v0;
-	v0.push_back(300*cos(45*PI/180.0));
-	v0.push_back(300*sin(45*PI/180.0));
+	//Parte uno
+	float angulo = 45.0;
+	float distancia45 = calcularMovimientoProyectil(angulo,"datos1.dat");
+	cout<<"(Punto 1) La distancia recorrida es: "<<distancia45<<"m"<<endl;
 
-	calcularMovimientoProyectil(x0,v0);
+	//Punto dos
+	float distanciasAngulos[7];
+	for (int i = 1; i <= 7; ++i)
+	{
+		angulo=10*i;
+		distanciasAngulos[i-1]=calcularMovimientoProyectil(angulo,"datos2.dat");
+	}
+
+	float distanciaMaxima = 0;
+	int anguloMaximo = 10;
+	for (int i = 0; i < 7; ++i)
+	{
+		if (distanciasAngulos[i]>distanciaMaxima)
+		{
+			distanciaMaxima = distanciasAngulos[i];
+			anguloMaximo = (i+1)*10;
+		}
+	}
+	cout<<"(Punto 2) La distancia maxima recorrida es: "<<distanciaMaxima<<"m"<<" y se da para un angulo de "<<anguloMaximo<<" grados."<<endl;
 	return 0;
 }
