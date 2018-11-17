07378043e8d623e3a3500298b0101c45a376e387
JADC362 <32496764+JADC362@users.noreply.github.com>
2018-11-17 12:46:59 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -21,7 +21,9 @@ static float dt = alpha*pow(dx,2)/v;
 
 int N = (int) L/dx;
 int NBarra = (int) d/dx;
-int T = 1000;
+int TF = 1000;
+int TA = 1000;
+int TP = 1000;
 
 void ecuacionDifusionCondicionesF(string nombreArchivo){
 	//Creacion archivo de datos
@@ -45,8 +47,8 @@ void ecuacionDifusionCondicionesF(string nombreArchivo){
 			}
 		}
 	}
-	datos << "T,"<<T<<",N,"<<N<<"\n";
-	for (int k = 0; k < T; ++k)
+	datos << "T,"<<TF<<",N,"<<N<<"\n";
+	for (int k = 0; k < TF; ++k)
 	{
 		datos << "Tiempo:"<<k*dt<<"\n";
 		for (int i = 0; i < N; ++i)
@@ -104,8 +106,8 @@ void ecuacionDifusionCondicionesA(string nombreArchivo){
 			}
 		}
 	}
-	datos << "T,"<<T<<",N,"<<N<<"\n";
-	for (int k = 0; k < T; ++k)
+	datos << "T,"<<TA<<",N,"<<N<<"\n";
+	for (int k = 0; k < TA; ++k)
 	{
 		datos << "Tiempo:"<<k*dt<<"\n";
 		for (int i = 0; i < N; ++i)
@@ -147,8 +149,72 @@ void ecuacionDifusionCondicionesA(string nombreArchivo){
 	datos.close();
 }
 
+void ecuacionDifusionCondicionesP(string nombreArchivo){
+	//Creacion archivo de datos
+	ofstream datos;
+	datos.open(nombreArchivo.c_str());
+
+	float matrizPasado[N][N];
+	float matrizPresente[N][N];
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			float eqCirc = pow((i*dx)-(L/2),2)+pow((j*dx)-(L/2),2);
+			if (eqCirc <= pow(d/2,2))
+			{
+				matrizPasado[i][j]=100;
+			}
+			else{
+				matrizPasado[i][j]=10;
+			}
+		}
+	}
+	datos << "T,"<<TP<<",N,"<<N<<"\n";
+	for (int k = 0; k < TP; ++k)
+	{
+		datos << "Tiempo:"<<k*dt<<"\n";
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{	
+				if ((i > 0  && i < (N-1)) && (j > 0 && j < (N-1)))
+				{
+					float eqCirc = pow((i*dx)-(L/2),2)+pow((j*dx)-(L/2),2);
+					if (eqCirc > pow(d/2,2))
+					{
+						matrizPresente[i][j]=alpha*(matrizPasado[i+1][j]+matrizPasado[i-1][j]+matrizPasado[i][j+1]+matrizPasado[i][j-1])+((1-4*alpha)*matrizPasado[i][j]);
+					}
+					else{
+						matrizPresente[i][j] = 100;
+					}
+				}
+				else{
+					for (int i = 0; i < N; ++i)
+					{
+						matrizPresente[0][i]=matrizPresente[N-1][i];
+						matrizPresente[i][0]=matrizPresente[i][N-1];
+					}
+				}
+				datos << matrizPasado[i][j]<< ",";
+			}
+			datos << "\n";
+		}
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				matrizPasado[i][j]=matrizPresente[i][j];
+			}
+		}
+	}
+	datos.close();
+}
+
 int main(){
 	ecuacionDifusionCondicionesF("datosF.dat");
 	ecuacionDifusionCondicionesA("datosA.dat");
+	ecuacionDifusionCondicionesP("datosP.dat");
 	return 0;
 }
