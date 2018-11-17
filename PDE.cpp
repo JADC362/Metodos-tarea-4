04492547ca6d88421fb0631cae57a7be06d2f78d
JADC362 <32496764+JADC362@users.noreply.github.com>
2018-11-17 11:48:40 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -21,7 +21,7 @@ static float dt = alpha*pow(dx,2)/v;
 
 int N = (int) L/dx;
 int NBarra = (int) d/dx;
-int T = 100;
+int T = 1000;
 
 void ecuacionDifusionCondicionesF(string nombreArchivo){
 	//Creacion archivo de datos
@@ -82,7 +82,73 @@ void ecuacionDifusionCondicionesF(string nombreArchivo){
 	datos.close();
 }
 
+void ecuacionDifusionCondicionesA(string nombreArchivo){
+	//Creacion archivo de datos
+	ofstream datos;
+	datos.open(nombreArchivo.c_str());
+
+	float matrizPasado[N][N];
+	float matrizPresente[N][N];
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			float eqCirc = pow((i*dx)-(L/2),2)+pow((j*dx)-(L/2),2);
+			if (eqCirc <= pow(d/2,2))
+			{
+				matrizPasado[i][j]=100;
+			}
+			else{
+				matrizPasado[i][j]=10;
+			}
+		}
+	}
+	datos << "T,"<<T<<",N,"<<N<<"\n";
+	for (int k = 0; k < T; ++k)
+	{
+		datos << "Tiempo:"<<k*dt<<"\n";
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{	
+				if ((i > 0  && i < (N-1)) && (j > 0 && j < (N-1)))
+				{
+					float eqCirc = pow((i*dx)-(L/2),2)+pow((j*dx)-(L/2),2);
+					if (eqCirc > pow(d/2,2))
+					{
+						matrizPresente[i][j]=alpha*(matrizPasado[i+1][j]+matrizPasado[i-1][j]+matrizPasado[i][j+1]+matrizPasado[i][j-1])+((1-4*alpha)*matrizPasado[i][j]);
+					}
+					else{
+						matrizPresente[i][j] = 100;
+					}
+				}
+				else{
+					for (int i = 0; i < N; ++i)
+					{
+						matrizPresente[0][i]=matrizPresente[1][i];
+						matrizPresente[N-1][i]=matrizPresente[N-2][i];
+						matrizPresente[i][0]=matrizPresente[i][1];
+						matrizPresente[i][N-1]=matrizPresente[i][N-2];
+					}
+				}
+				datos << matrizPasado[i][j]<< ",";
+			}
+			datos << "\n";
+		}
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				matrizPasado[i][j]=matrizPresente[i][j];
+			}
+		}
+	}
+	datos.close();
+}
+
 int main(){
 	ecuacionDifusionCondicionesF("datosF.dat");
+	ecuacionDifusionCondicionesA("datosA.dat");
 	return 0;
 }
